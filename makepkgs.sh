de0076af25485d977620377aa34ddf9de244b57f
Greg Fitzgerald <gregf@hugops.pw>
2020-02-15 15:32:42 +0000

    Update for modern arch use
    * I adopted patches from the original repo to remove the root requirement
    * Removed i686 support
    * fixed aur snapshot urls

--- makepkgs.sh
+++ makepkgs.sh
@@ -12,7 +12,7 @@ As soon as the program sees one of these options, all command line
 argument processing stops.
 
   -h  Show this help
-  -l  Only list package info. 
+  -l  Only list package info.
   -d  Only reconstruct repo database.
   -s  Only sign packages.
 
@@ -35,12 +35,12 @@ COMBINABLE FLAGS:
   -u  <USERNAME>   User to run package signing as.  Defaults to \$USER,
                    which retains orignal user name even with privilege
                    escalation with su.
-  
+
   -f  Update RSS   Update an RSS feed
 
 END_OF_SHOW_HELP
-} 
- 
+}
+
 ### CONSTANTS & SHORTCUTS ###
 
 COLOR='\e[1;35m'
@@ -54,6 +54,7 @@ TAB3='tput cub 80; tput cuf 80'
 
 NEWPKS=""
 BADPKS=""
+LOCKFILE="/tmp/s.lock"
 FLAG_MAKE=true  # Make packages?
 FLAG_LIST=false # Only list package info?
 FLAG_REPO=false # Only update the repos?
@@ -84,25 +85,24 @@ done
 
 shift $((OPTIND-1))
 
-### CHECK TO SEE IF RUN AS ROOT ###
+### MAKE SURE THE SCRIPT ISN'T RUNNING WITH ROOT RIGHTS ###
 
-[[ ${EUID} -eq 0 && ! ${FLAG_MAKE} ]] &&
-  echo "This script must be run as root when making packages." >&2 && exit 1
+[[ ${EUID} -eq 0 ]] && echo -e "It's not allowed to build packages as root." && exit 1
 
 ### CHECK TO SEE IF WE ARE ALREADY RUNNING ###
 
-if [ -f /var/run/lock/makepkgs.lock ]; then
-  echo "Lock file /var/run/lock/makepkgs.lock detected.  Is the script already running?" >&2 
+if [ -f ${LOCKFILE} ]; then
+  echo "Lock file ${LOCKFILE} detected.  Is the script already running?" >&2
   [ -t 1 ] && echo "This file may be left behind if the script crashes or is interrupted"
   [ -t 1 ] && echo "If you are sure that this script is not running please delete the lock file."
   exit 1
 else
-  touch /var/run/lock/makepkgs.lock
+  touch ${LOCKFILE}
 fi
 
 ### TELL USER ABOUT FALLING BACK TO DEFAULTS ###
 
-[ -z ${CHROOT} ] && CHROOT="/srv/build" && $FLAG_INFO && [ -t 1 ] && echo "No chroot directory specified, defaulting to /srv/build" 
+[ -z ${CHROOT} ] && CHROOT="/srv/build" && $FLAG_INFO && [ -t 1 ] && echo "No chroot directory specified, defaulting to /srv/build"
 [ -z ${REPDIR} ] && REPDIR="/srv/repo" && $FLAG_INFO && [ -t 1 ] && echo "No repo directory specified, defaulting to /srv/repo"
 [ -z ${USRNAM} ] && USRNAM="${USER}" && $FLAG_INFO && [ -t 1 ] && echo "No username specified.  Will sign packages as ${USER}"
 
@@ -114,12 +114,11 @@ done
 
 ### MAKE SURE A REPO NAME WAS SPECIFIED ###
 
-[ -z ${REPNAM} ] && echo "No repo name specified" >&2 && show_help >&2 rm /var/run/lock/makepkgs.lock && exit 1
-
+[ -z ${REPNAM} ] && echo "No repo name specified" >&2 && show_help >&2 rm /var/run/lock/s.lock && exit 1
 
 ### MAKE SURE THE BUILD CHROOTS EXISTS ###
 
-for arch in x86_64 i686; do
+for arch in x86_64; do
   if [ ! -d "${CHROOT}/${arch}/root" ]; then
     echo "${CHROOT}/{$arch}/root does not exist.  Does ${CHROOT} contain chroots for building?" >&2
     exit 1
@@ -173,7 +172,7 @@ function pkg_ver_aur () {
 }
 
 function pkg_get () {
-  curl -s -o ${1}.tar.gz https://aur.archlinux.org/packages/${1:0:2}/${1}/${1}.tar.gz
+  curl -s -o ${1}.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/${1}.tar.gz
   tar -zxvf ${1}.tar.gz > /dev/null
   rm ${1}.tar.gz
 }
@@ -184,7 +183,7 @@ function pkg_remove() {
 }
 
 function pkg_add () {
-  # A bit hackish since some git builds will change pkg version after makepkg
+  # A bit hackish since some git builds will change pkg version after 
   # The only guarantee left is that there will a single package inside ${1}
   message "Moving ${1} package to repo..."
   mv -v ${REPDIR}/${REPNAM}/build/aur/${1}/*.pkg.tar.xz ${REPDIR}/${REPNAM}/${2}
@@ -196,12 +195,12 @@ function repo_build() {
     message "Updating the repo database for ${REPNAM}/${1}..."
     repo-add -q ${REPDIR}/${REPNAM}/${1}/${REPNAM}.db.tar.xz ${REPDIR}/${REPNAM}/${1}/*.pkg.tar.xz
     message "Updating the repo filelist for ${REPNAM}/${1}..."
-    repo-add -q -f ${REPDIR}/${REPNAM}/${1}/${REPNAM}.files.tar.xz ${REPDIR}/${REPNAM}/${1}/*.pkg.tar.xz
+    repo-add -q ${REPDIR}/${REPNAM}/${1}/${REPNAM}.files.tar.xz ${REPDIR}/${REPNAM}/${1}/*.pkg.tar.xz
   else
     message "Updating the repo database for ${REPNAM}/${1}..."
     su -c "repo-add -q ${REPDIR}/${REPNAM}/${1}/${REPNAM}.db.tar.xz ${REPDIR}/${REPNAM}/${1}/*.pkg.tar.xz" - ${USRNAM}
     message "Updating the repo filelist for ${REPNAM}/${1}..."
-    su -c "repo-add -q -f ${REPDIR}/${REPNAM}/${1}/${REPNAM}.files.tar.xz ${REPDIR}/${REPNAM}/${1}/*.pkg.tar.xz" - ${USRNAM}
+    su -c "repo-add -q ${REPDIR}/${REPNAM}/${1}/${REPNAM}.files.tar.xz ${REPDIR}/${REPNAM}/${1}/*.pkg.tar.xz" - ${USRNAM}
   fi
 }
 
@@ -222,7 +221,7 @@ function sign_pkgs() {
 function pkg_build () {
 
   message "Preparing to build ${1} for ${4}..."
-  rm -rf ${REPDIR}/${REPNAM}/build/aur/${1}
+  sudo rm -rf ${REPDIR}/${REPNAM}/build/aur/${1}
 
   mpec=1
   pkg_get ${1}
@@ -231,22 +230,17 @@ function pkg_build () {
   tnat="`echo ${trch} | grep ${4}`"
   tany="`echo ${trch} | grep any`"
 
-  if [ -n "${tany}" -a "${4}" == "i686" ]; then
-    message 'ANY package detected, will copy x86_64 version.'
-    cp ${REPDIR}/${REPNAM}/x86_64/${1}* ${REPDIR}/${REPNAM}/i686/
-    [[ "${2}" != "missing" ]] && pkg_remove ${1} ${2} ${4}
-  else
     if [ -n "${tnat}" -o -n "${tany}" ]; then
-      chown -R nobody ${REPDIR}/${REPNAM}/build/aur/${1}
+      chown -R ${USRNAM}:$(id -ng ${USRNAM}) ${REPDIR}/${REPNAM}/build/aur/${1}
       cd ${REPDIR}/${REPNAM}/build/aur/${1}
       [[ -f ../${1}.sh ]] && message 'Executing PKGBUILD customization...' && sh ../${1}.sh
       makechrootpkg -cur ${CHROOT}/${4} -l aurpbs
       if [ $? == 0 ]; then
         message 'Package creation succeeded!'
-        if [ -f "`ls ${REPDIR}/${REPNAM}/build/aur/${1}/${1}-*.pkg.tar 2> /dev/null`" ]; then 
+        if [ -f "`ls ${REPDIR}/${REPNAM}/build/aur/${1}/${1}-*.pkg.tar 2> /dev/null`" ]; then
           message 'Package left as tarball.  Manually compressing...'
           xz ${REPDIR}/${REPNAM}/build/aur/${1}/${1}-*.pkg.tar
-        fi 
+        fi
         [[ "${2}" != "missing" ]] && pkg_remove ${1} ${2} ${4}
         pkg_add ${1} ${4}; sign_pkgs ${4}; repo_build ${4}; system_update ${4};
         NEWPKS="${NEWPKS}${1} for ${4}"$'\n'
@@ -260,7 +254,6 @@ function pkg_build () {
       [ -t 1 ] && echo -e "\e[1;31mPACKAGE ${1} not intended for ${4}.${RESET}"
     fi
     rm -rf ${REPDIR}/${REPNAM}/build/aur/w${1}
-  fi
   return ${mpec}
 }
 
@@ -274,9 +267,9 @@ function pkg_search () {
   fi
 }
 
-${FLAG_SIGN} && { sign_pkgs x86_64; sign_pkgs i686; exit 0; }
-${FLAG_REPO} && { repo_build x86_64; repo_build i686; exit 0; }
-${FLAG_LIST} || { system_update x86_64 && system_update i686; }
+${FLAG_SIGN} && { sign_pkgs x86_64; exit 0; }
+${FLAG_REPO} && { repo_build x86_64; exit 0; }
+${FLAG_LIST} || { system_update x86_64; }
 
 ### MAKE SURE WE HAVE A PAKCAKGE LIST ###
 
@@ -285,9 +278,9 @@ if [ -f "${REPDIR}/${REPNAM}/build/aur/packages.list" ]; then
   ### CREATE DIRECTORIES IF THEY DON'T EXIST ###
 
   mkdir -p ${REPDIR}/${REPNAM}/build/aur
-  mkdir -p ${REPDIR}/${REPNAM}/{x86_64,i686}
+  mkdir -p ${REPDIR}/${REPNAM}/x86_64 && chown ${USRNAM}:$(id -ng ${USRNAM}) ${REPDIR}/${REPNAM}/x86_64
 
-  if [ -t 1 ]; then 
+  if [ -t 1 ]; then
     echo -e "${BEGIN}*** STARTING WITH REPO ${REPNAM} ***${RESET}\n"
     echo -ne "PACKAGE NAME"; eval ${TAB1}
     echo -ne "LCL X86_64 VER"; eval ${TAB2}
@@ -301,35 +294,33 @@ if [ -f "${REPDIR}/${REPNAM}/build/aur/packages.list" ]; then
     for pkg in ${line}; do
       [[ "${pkg:0:1}" == "#" ]] && break
       cd ${REPDIR}/${REPNAM}/build/aur
-      lvx=$(pkg_ver_loc ${pkg} x86_64); lvi=$(pkg_ver_loc ${pkg} i686)
+      lvx=$(pkg_ver_loc ${pkg} x86_64)
       if [ ${depupd} == 1 ]; then
         message "Dependency of ${pkg} updated.  Clearing out for rebuild..."
-        pkg_remove ${pkg} \* "{x86_64,i686}"
-        repo_build x86_64; repo_build i686
-        system_update x86_64; system_update i686
-        lvx=$(pkg_ver_loc ${pkg} x86_64); lvi=$(pkg_ver_loc ${pkg} i686)
+        pkg_remove ${pkg} \* "x86_64"
+        repo_build x86_64
+        system_update x86_64
+        lvx=$(pkg_ver_loc ${pkg} x86_64)
       fi
-      if [ -t 1 ]; then 
+      if [ -t 1 ]; then
         echo -ne "${COLOR}${pkg}${RESET}"; eval $TAB1
         echo -ne "${COLOR}${lvx}${RESET}"; eval $TAB2
-        echo -ne "${COLOR}${lvi}${RESET}"; eval $TAB3
       fi
       av=$(pkg_ver_aur ${pkg})
       [ -t 1 ] && echo -e "${COLOR}${av}${RESET}"
       if [ ${av} == missing ]; then
-        pkg_search ${pkg} \* "{x86_64,i686}"
+        pkg_search ${pkg} \* "x86_64"
         message "Removing ${1} from the repos..."
         rm -rf "${REPDIR}/${REPNAM}/build/aur/${pkg}" 2> /dev/null
       else
-        for arch in x86_64 i686; do
-          [ $arch == x86_64 ] && lvl=${lvx} || lvl=${lvi}
+          lvl=${lvx}
           if [ ${lvl} == missing ]; then
             if [ ${FLAG_LIST} == false ]; then
               pkg_build ${pkg} ${lvl} ${av} ${arch} && [[ $? == 0 ]] && depupd=1
             else
               $FLAG_INFO && [ -t 1 ] && echo "List mode on...not building missing ${arch} package."
             fi
-          else 
+          else
             if pkg_ver_comp ${lvl} ${av}; then
               if [ ${FLAG_LIST} == false ]; then
                 pkg_build ${pkg} ${lvl} ${av} ${arch} && [[ $? == 0 ]] && depupd=1
@@ -338,7 +329,6 @@ if [ -f "${REPDIR}/${REPNAM}/build/aur/packages.list" ]; then
               fi
             fi
           fi
-        done
       fi
     done
   done < "${REPDIR}/${REPNAM}/build/aur/packages.list"
@@ -351,20 +341,20 @@ fi
 
 if [ ! ${FLAG_URSS} ]; then
 
-  # This section relies on BrainwreckedRSS 
+  # This section relies on BrainwreckedRSS
   # Visit rss.bw-tech.net for more information
 
   cd ${REPDIR}/bwrss
 
-  [[ "$NEWPKS" != "" ]] && 
+  [[ "$NEWPKS" != "" ]] &&
     NEWPKS=$(echo "$NEWPKS" | sed ':a;N;$!ba;s/\n/\&lt;br\/\&gt; /g') &&
     php update.php aurpb "New Packages Built" "AURPB Build Script" "${NEWPKS} &lt;br /&gt;Packages are waiting production."
 
-  [[ "$BADPKS" != "" ]] && 
+  [[ "$BADPKS" != "" ]] &&
     BADPKS=$(echo "$BADPKS" | sed ':a;N;$!ba;s/\n/\&lt;br\/\&gt; /g') &&
-    EXCUSE="Common reasons: Dependencies broken, source dl link broken, AUR maintainer broken." 
+    EXCUSE="Common reasons: Dependencies broken, source dl link broken, AUR maintainer broken."
     php update.php aurpb "Failed Packages" "AURPB Build Script" "${BADPKS} &lt;br /&gt;${EXCUSE}"
 fi
 
-rm /var/run/lock/makepkgs.lock
+rm ${LOCKFILE}
 exit 0
